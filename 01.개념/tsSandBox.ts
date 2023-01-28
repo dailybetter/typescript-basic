7061d5a3a2b7a2c49c4d30222b80d25286d830ff
dailybetter <a40293865@gmail.com>
2023-01-28 23:41:42 +0900

    1장 타입스크립트의 개념

---
+++ 01.개념/tsSandBox.ts
@@ -0,0 +1,4 @@
+const myName = 'HyunWoo'
+// let nameLength :number = myName.length();
+/*length 함수를 실행 할 때 js 에서는 코드를 실행하기 전까지 에러를 표출해주지 않지만
+타입스크립트에서는 Type 'Number' has no call signatures 라는 에러를 표출해 준다. */
